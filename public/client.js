300e06d14b79959aa469807544e8cb80da0298b3
zwillerzoog <marsh.heidi.c@gmail.com>
2017-07-20 14:07:30 -0400

    client.js

--- public/client.js
+++ public/client.js
@@ -1,14 +1,30 @@
-const url = 'http://localhost:8080/api';
 
 $(function() {
-  $('#query-form').submit(function(e){
+  $('#register-form').submit(function(e){
     e.preventDefault();
     console.log('Hello');
-    const query = $('#query').val();
-    console.log(query);
+    const url = 'http://localhost:8080/api/signup';
+    const usernameInput = $('#username-input').val();
+    console.log("username: ", usernameInput);
     $.ajax({
       url, 
       method: 'POST',
+      //dataType: "json",
+      data: {usernameInput}
+    })
+      .done(data => {
+        console.log(data);
+      });
+  });
+    $('#query-form').submit(function(e){
+    e.preventDefault();
+    console.log('Hello');
+    const url = 'http://localhost:8080/api/:id';
+    const query = $('#query').val();
+    console.log("query: ", query);
+    $.ajax({
+      url, 
+      method: 'PUT',
       //dataType: "json",
       data: {query}
     })
@@ -17,3 +33,4 @@ $(function() {
       });
   });
 });
+
--- server.js
+++ server.js
@@ -18,6 +18,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  //res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'GET', 'DELETE');
+  next();
+});
+
 const basicStrategy = new BasicStrategy(function (username, password, done) {
   let user;
   console.log(username, 'username');
